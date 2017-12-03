06615a566a10d5194c612b15a3e27aeb50f1df75
mcguffin <joern@flyingletters.com>
2017-12-03 13:42:11 +0100

    Release version 1.1.16

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ Plugin Name: GitUpdate Test
 Plugin URI: https://github.com/mcguffin/gitupdate-test
 Description: Enter description here.
 Author: Jörn Lund
-Version: 1.1.15
+Version: 1.1.16
 Author URI: https://github.com/mcguffin
 License: GPL3
 Github Repository: mcguffin/gitupdate-test
